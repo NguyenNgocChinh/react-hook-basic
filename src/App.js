bfc5a7708a78cf1b490d3b8d2164fc4a590365ab
Chinh <chinhnguyen.210399@gmail.com>
2020-08-12 08:30:39 +0700

    useEffect - Call API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import TodoList from './components/TodoList';
-import FormTodoList from './components/FormTodoList';
+import PostList from './components/PostList';
 
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: 'Buoc 1' },
-    { id: 2, title: 'Buoc 2' },
-    { id: 3, title: 'Buoc 3' },
-    { id: 4, title: 'Buoc 4' },
-    { id: 5, title: 'Buoc 5' }
-  ]);
-  function handleTodoClick(todo) {
-    console.log(todo);
-    const index = todoList.findIndex(x => x.id === todo.id);
-    if (index < 0) return;
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  }
 
-  function handleFormSubmit(formvalues) {
-    const newToDoList = [...todoList];
-    newToDoList.push({
-      id: todoList.length + Math.trunc(Math.random() * 23),
-      ...formvalues
-    });
+  const [postsList, setPostsList] = useState([]);
 
-    setTodoList(newToDoList);
-  }
+  useEffect(() => {
+    async function fetchPostList() {
+      const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+      const reponse = await fetch(requestUrl);
+      const reponseJSON = await reponse.json();
+      const { data } = reponseJSON;
+      setPostsList(data);
+    }
+    fetchPostList();
+  }, []);
 
   return (
     <div className="app">
-      <h1>React Hook - TodoList</h1>
-      <FormTodoList onSubmitForm={handleFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <h1>React Hook - useEffect - Call API</h1>
+      <PostList posts={postsList} />
     </div>
   );
 }
